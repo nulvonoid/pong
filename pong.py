b4c6e7f9081109de10c0dc46718b88ffb1fecb13
nulvonoid <nulvonoid@gmail.com>
2020-05-10 01:30:05 +0700

    raketbisa gerak

--- pong.py
+++ pong.py
@@ -9,7 +9,7 @@ layar.bgcolor("black")
 layar.setup(width=800, height=800)
 layar.tracer(0)
 
-#raket A
+#raket A kiri
 raket_a = turtle.Turtle()
 raket_a.speed(0)
 raket_a.shape("square")
@@ -18,7 +18,7 @@ raket_a.shapesize(stretch_wid=5,stretch_len=1)
 raket_a.penup()
 raket_a.goto(-350,0)
 
-#raket B
+#raket B kanan
 raket_b = turtle.Turtle()
 raket_b.speed(0)
 raket_b.shape("square")
@@ -35,6 +35,37 @@ bola.shape("circle")
 bola.color("white")
 bola.penup()
 bola.goto(0,0)
+
+#function
+def raket_a_atas():
+    y = raket_a.ycor()
+    y +=20
+    raket_a.sety(y)
+
+def raket_a_bawah():
+    y = raket_a.ycor()
+    y -=20
+    raket_a.sety(y)
+
+def raket_b_atas():
+    y = raket_b.ycor()
+    y +=20
+    raket_b.sety(y)
+
+def raket_b_bawah():
+    y = raket_b.ycor()
+    y -=20
+    raket_b.sety(y)
+
+
+
+#kibord
+layar.listen()
+layar.onkeypress(raket_a_atas, "w")                 #konsensus kalo huruf kecil, kalo arrow keys kapital awalnya
+layar.onkeypress(raket_a_bawah, "s")
+layar.onkeypress(raket_b_atas, "Up")
+layar.onkeypress(raket_b_bawah, "Down")
+
 
 #loop utama
 while True:                                     #awalmya raket ga muncul, ternyata ini harus ditaro di bagian akhir 
